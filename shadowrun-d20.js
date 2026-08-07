1e13c1d1083e976dd523cf7e6d34f81a00799455
MarcioBADias <marcio.b.a.dias@gmail.com>
2026-08-07 02:59:54 -0300

    Conversão de DnD A Shadownrun 1.8

--- shadowrun-d20.js
+++ shadowrun-d20.js
@@ -1,15 +1,26 @@
 import { ShadowrunActorSheet } from "./module/sheets/actor-sheet.js";
 
+// Função para carregar os templates Handlebars na memória
+async function preloadHandlebarsTemplates() {
+  const templatePaths = [
+    "systems/shadowrun-d20-5e/templates/actor/character-sheet.hbs",
+  ];
+  return loadTemplates(templatePaths);
+}
+
 Hooks.once("init", async function () {
   console.log("Shadowrun D20 5e | Inicializando Sistema");
 
-  // 1. Unregistra a ficha padrão genérica do Foundry
+  // 1. Desregistra fichas genéricas padrão
   Actors.unregisterSheet("core", ActorSheet);
 
-  // 2. Registra a ficha customizada do seu Shadowrun
+  // 2. Registra a ficha customizada
   Actors.registerSheet("shadowrun-d20-5e", ShadowrunActorSheet, {
     types: ["character"],
     makeDefault: true,
     label: "Ficha Shadowrun D20 5e",
   });
+
+  // 3. Pré-carrega os arquivos HBS
+  return preloadHandlebarsTemplates();
 });
